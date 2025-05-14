ba795b5b9f0e6e2c514d811ab06d2b17c8b3eae9
Darkxh <daviddo740@gmail.com>
2025-05-13 23:00:27 -0700

    cleaning up code

--- main.py
+++ main.py
@@ -7,29 +7,17 @@ import datetime
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-##Set your prefix and intents
 intents = discord.Intents.default()
 intents.message_content = True
 
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-##Replace this with the channel ID you want the bot to message
-CHANNEL_ID = 769447115285463040  # example: 987654321098765432
 
+CHANNEL_ID = 769447115285463040  
 START_HOUR = 9
 END_HOUR = 2
 
-#def shutdown_bot():
-    # Close the bot gracefully
- #   asyncio.run_coroutine_threadsafe(bot.close(), bot.loop)
-
-#def run_schedule():
-#    while True:
-#        schedule.run_pending()
-#        time.sleep(1)
-
-#schedule.every().day.at("23:00").do(shutdown_bot)
 
 def is_within_allowed_hours():
     now = datetime.datetime.now().time()
